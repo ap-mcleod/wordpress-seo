651b6d9d4e76055fc22b18d60578baa91e38126a
dennis <dennis@yoast.com>
2024-01-08 17:00:32 +0100

    Use `jest.spyOn` to replace missing global `spyOn`

--- packages/yoastseo/spec/worker/AnalysisWebWorkerSpec.js
+++ packages/yoastseo/spec/worker/AnalysisWebWorkerSpec.js
@@ -167,7 +167,7 @@ describe( "AnalysisWebWorker", () => {
 
 			test( "calls logger debug", () => {
 				const logger = getLogger( "yoast-analysis-worker" );
-				const spy = spyOn( logger, "debug" );
+				const spy = jest.spyOn( logger, "debug" );
 
 				scope.onmessage( createMessage( "initialize" ) );
 				expect( spy ).toHaveBeenCalledTimes( 2 );
@@ -464,7 +464,7 @@ describe( "AnalysisWebWorker", () => {
 
 			test( "calls analyze", done => {
 				const paper = new Paper( "This is the content." );
-				const spy = spyOn( worker, "analyze" );
+				const spy = jest.spyOn( worker, "analyze" );
 
 				worker.analyzeDone = () => {
 					expect( spy ).toHaveBeenCalledTimes( 1 );
@@ -732,7 +732,7 @@ describe( "AnalysisWebWorker", () => {
 			test( "calls analyzeRelatedKeywords", done => {
 				const paper = new Paper( "This is the content." );
 				const relatedKeywords = { a: { keyword: "content", synonyms: "" } };
-				const spy = spyOn( worker, "analyzeRelatedKeywords" );
+				const spy = jest.spyOn( worker, "analyzeRelatedKeywords" );
 
 				worker.analyzeRelatedKeywordsDone = () => {
 					expect( spy ).toHaveBeenCalledTimes( 1 );
@@ -858,7 +858,7 @@ describe( "AnalysisWebWorker", () => {
 
 			test( "calls loadScript", done => {
 				const payload = { url: "http://example.com" };
-				const spy = spyOn( worker, "loadScript" );
+				const spy = jest.spyOn( worker, "loadScript" );
 
 				worker.loadScriptDone = () => {
 					expect( spy ).toHaveBeenCalledTimes( 1 );
@@ -958,7 +958,7 @@ describe( "AnalysisWebWorker", () => {
 			test( "calls customMessage", done => {
 				const name = "test";
 				const payload = { name, data: { test: true } };
-				const spy = spyOn( worker, "customMessage" );
+				const spy = jest.spyOn( worker, "customMessage" );
 
 				worker._registeredMessageHandlers[ name ] = ( data ) => data;
 				worker.customMessageDone = () => {
@@ -1070,7 +1070,7 @@ describe( "AnalysisWebWorker", () => {
 			test( "calls runResearch", done => {
 				const name = "test";
 				const payload = { name };
-				const spy = spyOn( worker, "runResearch" );
+				const spy = jest.spyOn( worker, "runResearch" );
 
 				worker.runResearchDone = () => {
 					expect( spy ).toHaveBeenCalledTimes( 1 );
